44f63c85b867e50f9f2039138e696fdfb92e2443
AnsonAMS <anson_tohata@hotmail.com>
2020-12-24 20:08:30 +0100

    Adding easy filter method

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,8 @@
 const scores = [10, 30, 15, 25, 50, 40, 5];
+
+// filter all results above 20 and put these in new array filteredScores
+const filteredScores = scores.filter((score) => {
+    return score > 20;
+});
+
+console.log(scores)
