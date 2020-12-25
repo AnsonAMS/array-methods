4edab3623cab5b28ba891200274a50d5612a9c7a
AnsonAMS <anson_tohata@hotmail.com>
2020-12-25 19:23:39 +0100

    Adding map method

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,4 @@
-const scores = [10, 30, 15, 25, 50, 40, 5];
+// const scores = [10, 30, 15, 25, 50, 40, 5];
 
 // filter all results above 20 and put these in new array filteredScores
 // const filteredScores = scores.filter((score) => {
@@ -9,13 +9,21 @@ const scores = [10, 30, 15, 25, 50, 40, 5];
 
 // filter all premium users and put these in a new array premiumUsers
 
-const users = [
-    {name: 'Shaun', premium: true},
-    {name: 'Yoshi', premium: false},
-    {name: 'Mario', premium: false},
-    {name: 'Chun-Li', premium: true}
-];
+// const users = [
+//     {name: 'Shaun', premium: true},
+//     {name: 'Yoshi', premium: false},
+//     {name: 'Mario', premium: false},
+//     {name: 'Chun-Li', premium: true}
+// ];
 
-const premiumUsers = users.filter(user => user.premium);
+// const premiumUsers = users.filter(user => user.premium);
 
-console.log(premiumUsers);
+// console.log(premiumUsers);
+
+// map method divide all numbers by 2
+
+const prices = [ 20, 10, 30, 25, 15, 40, 80, 5 ];
+
+const salePrices = prices.map(price => price / 2);
+
+console.log(salePrices);
