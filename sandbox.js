64fade01c80a61a5e1e29784b288242239905e41
AnsonAMS <anson_tohata@hotmail.com>
2020-12-27 16:30:54 +0100

    Learning using reduce method with Lewandowski

--- sandbox.js
+++ sandbox.js
@@ -22,26 +22,39 @@
 
 // map method divide all numbers by 2
 
-const prices = [ 20, 10, 30, 25, 15, 40, 80, 5 ];
+// const prices = [ 20, 10, 30, 25, 15, 40, 80, 5 ];
 
-const salePrices = prices.map(price => price / 2);
+// const salePrices = prices.map(price => price / 2);
 
-console.log(salePrices);
+// console.log(salePrices);
 
-const products = [
-    { name: "gold star", price: 20 },
-    { name: "mushroom", price: 40 },
-    { name: "green shells", price: 30 },
-    { name: "banana skin", price: 10 },
-    { name: "red shells", price: 50 }
-];
+// const products = [
+//     { name: "gold star", price: 20 },
+//     { name: "mushroom", price: 40 },
+//     { name: "green shells", price: 30 },
+//     { name: "banana skin", price: 10 },
+//     { name: "red shells", price: 50 }
+// ];
 
-const saleProducts = products.map(product => {
-    if (product.price > 30) {
-        return { name: product.name, price: product.price / 2 };
-    } else {
-        return product;
+// const saleProducts = products.map(product => {
+//     if (product.price > 30) {
+//         return { name: product.name, price: product.price / 2 };
+//     } else {
+//         return product;
+//     }
+// })
+
+// console.log(saleProducts, products)
+
+// reducemethod how many times did Lewandowski score more than 3 or more goals per game? 
+
+const goalsScoredByLewandowski = [ 3, 2, 1, 1, 0, 5, 4, 3 ];
+
+const threeOrMoreGoalsPerGame = goalsScoredByLewandowski.reduce((acc, curr) => {
+    if (curr >= 3) {
+        acc++;
     }
-})
+    return acc;
+}, 0);
 
-console.log(saleProducts, products)
+console.log(threeOrMoreGoalsPerGame);
